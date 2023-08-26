0a667a30b5fc762030ea85685ec8c6ff1e484791
Wallace-Bezerra <wallace_2014_@hotmail.com>
2023-08-26 19:53:52 -0300

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,28 @@
-import ClientComponent from '@/lib/ClientComponent'
-import StyledComponentsRegistry from '@/lib/registry'
-import { Header } from '@/components/Header'
-import { GlobalStyle } from '@/styles/GlobalStyle'
-import { Rubik, Barlow } from 'next/font/google'
-import type { Metadata } from 'next'
-import Head from 'next/head'
-import { Footer } from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/react";
+import ClientComponent from "@/lib/ClientComponent";
+import StyledComponentsRegistry from "@/lib/registry";
+import { Header } from "@/components/Header";
+import { GlobalStyle } from "@/styles/GlobalStyle";
+import { Rubik, Barlow } from "next/font/google";
+import type { Metadata } from "next";
+import Head from "next/head";
+import { Footer } from "@/components/Footer";
 
-const rubik = Rubik({ subsets: ['latin'], weight: ["300", '400', '500', '600', '700'] })
-const barlow = Barlow({ subsets: ['latin'], style: 'normal', weight: ["300", '400', '500', '600', '700'] })
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+const barlow = Barlow({
+  subsets: ["latin"],
+  style: "normal",
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
-  title: 'Wallace Bezerra | Desenvolvedor Front-end',
-  description: 'Desenvolvedor Front-end com Experiência em React.js , Next.js , TypeScript,  Styled Components, Tailwind.',
-}
+  title: "Wallace Bezerra | Desenvolvedor Front-end",
+  description:
+    "Desenvolvedor Front-end com Experiência em React.js , Next.js , TypeScript,  Styled Components, Tailwind.",
+};
 
 export default function RootLayout({
   children,
@@ -21,21 +30,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html style={{ position: 'relative' }} lang="pt-br">
+    <html style={{ position: "relative" }} lang="pt-br">
       <Head>
         <link
           rel="preload"
           href="https://unpkg.com/swiper/css/swiper.min.css"
-          as='style'
+          as="style"
         />
       </Head>
       <body>
-      {/* <body className={`${rubik.className} ${barlow.className}`}> */}
+        {/* <body className={`${rubik.className} ${barlow.className}`}> */}
         <ClientComponent>
           <StyledComponentsRegistry>
             <GlobalStyle />
             <Header />
             {children}
+            <Analytics />
             <Footer />
           </StyledComponentsRegistry>
         </ClientComponent>
@@ -43,4 +53,3 @@ export default function RootLayout({
     </html>
   );
 }
-
